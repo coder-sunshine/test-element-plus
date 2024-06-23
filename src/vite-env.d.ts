470a7405c7fb005358b2e20090c450a9b74ef0d7
coder-sunshine <283953455@qq.com>
2024-06-23 18:13:20 +0800

    feat: init

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,11 @@
-/// <reference types="../node_modules/element-plus/global.d.ts" />
-
 /// <reference types="ant-design-vue/typings/global" />
 
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
+/// <reference types="element-plus/global" />
 
-  const component: DefineComponent<{}, {}, any>
+// declare module '*.vue' {
+//   import { DefineComponent } from 'vue'
 
-  export default component
-}
+//   const component: DefineComponent<{}, {}, any>
+
+//   export default component
+// }
